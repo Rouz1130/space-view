4ef45ada32a32f37338e48990f562b8874d7a31a
Joel Waage and Rouz Majlessi <joel+rouz@>
2016-08-17 11:46:45 -0700

    create shell complete with gulpfile.js full populated all the way through scss

---
+++ js/space-interface.js
@@ -0,0 +1,8 @@
+var View = require('./../js/space.js').viewModule;
+
+$(document).ready(function() {
+  $('#placeholder').submit(function(event) {
+    event.preventDefault();
+    return true;
+  });
+});
---
+++ js/space.js
@@ -0,0 +1,9 @@
+function View() {
+
+}
+
+View.prototype.blank = function(placeholder) {
+  var something;
+};
+
+exports.viewModule = View;
